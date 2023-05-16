6a32c2dcf7d7254d8a3226afaef0fcc0138cbee5
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-05-16 17:05:54 +0100

    added images of animals, animal questions in new file, added basic styling for state pages

---
+++ questions.js
@@ -0,0 +1,67 @@
+export const QueenslandQuestions = [
+  {
+    question: "Which of the following adaptations do cassowaries possess?",
+    answers: [
+      "Echolocation abilities",
+      "Venomous spines",
+      "Hollow bones for buoyancy",
+      "Camouflage capabilities",
+    ],
+    correct: "Hollow bones for buoyancy",
+    badge: "cassowary",
+  },
+  {
+    question: "What is the diet of the inland taipan primarily composed of?",
+    answers: [
+      "Small mammals",
+      "Birds and their eggs",
+      "Insects and spiders",
+      "Aquatic prey, such as fish",
+    ],
+    correct: "Small mammals",
+    badge: "inland-taipan",
+  },
+  {
+    question: "What is the primary habitat of crocodiles?",
+    answers: [
+      "Dense rainforests",
+      "Arctic tundra",
+      "Open grasslands",
+      "Freshwater and brackish habitats",
+    ],
+    correct: "Freshwater and brackish habitats",
+    badge: "crocodile",
+  },
+];
+
+export const NorthernTerritoryQuestions = [
+  {
+    question: "Which of the following pythons is native to Australia?",
+    answers: [
+      "Reticulated python",
+      "Burmese python",
+      "Green tree python",
+      "Ball python",
+    ],
+    correct: "Green tree python",
+    badge: "python",
+  },
+  {
+    question:
+      "Which of the following species of wallaby is considered the largest?",
+    answers: [
+      "Agile wallaby",
+      "Brush-tailed rock wallaby",
+      "Red-necked wallaby",
+      "Whiptail wallaby",
+    ],
+    correct: "Red-necked wallaby",
+    badge: "wallaby",
+  },
+  {
+    question: "What is the scientific name of the cane toad?",
+    answers: ["Rhinella marina", "Bufo bufo", "Hyla arborea", "Xenopus laevis"],
+    correct: "Rhinella marina",
+    badge: "cane-toad",
+  },
+];
