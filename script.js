32738b176ca775aa660044b349a5041762354bfa
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-05-16 17:20:20 +0100

    added timer and basic game functionality

---
+++ script.js
@@ -0,0 +1,51 @@
+import {
+  QueenslandQuestions,
+  NorthernTerritoryQuestions,
+} from "./questions.js";
+
+const question = document.getElementById("question");
+const questionContainer = document.querySelector(".question-container");
+const quizBtn = document.getElementById("quizbtn");
+const answers = document.getElementById("answers");
+
+function startQuiz() {
+  quizBtn.remove();
+
+  nextQuestion(0);
+}
+
+async function nextQuestion(i) {
+  if (chosenQuestions.length <= i) {
+    await delayTimer(500);
+    endQuiz();
+    return;
+  }
+
+  question.textContent = chosenQuestions[i].question;
+
+  const currentAnswers = chosenQuestions[i].answers;
+
+  const html = `  <button class="btn">${currentAnswers[0]}</button>
+    <button class="btn">${currentAnswers[1]}</button>
+    <button class="btn">${currentAnswers[2]}</button>
+    <button class="btn">${currentAnswers[3]}</button>`;
+  answers.innerHTML = html;
+
+  const answerBtns = Array.from(answers.querySelectorAll(".btn"));
+
+  answerBtns.forEach((button) => {
+    button.addEventListener("click", (e) => checkAnswer(e, i));
+  });
+}
+
+// HELPER FUNCTIONS
+
+function delayTimer(delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
+  });
+}
+
+quizBtn.addEventListener("click", startQuiz);
