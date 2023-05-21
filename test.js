c4bddb26e8cce87b1847f918616b6a24b097c50f
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-05-21 23:03:00 +0100

    reworked question script for API

--- test.js
+++ test.js
@@ -1,38 +1,39 @@
 const gameForm = document.getElementById("game-form");
 const submitButton = document.getElementById("submitButton");
+const questionText = document.getElementById("question");
+const answersText = document.getElementById("answers");
 
 const url = window.location.search.split("?");
 
 let category;
+let correctAnswers = 0;
 
 switch (url[1]) {
   case "history":
     category = "23";
     break;
   case "geography":
-    dayName = "22";
+    category = "22";
     break;
   case "film":
-    dayName = "11";
+    category = "11";
     break;
   case "science":
-    dayName = "17";
+    category = "17";
     break;
   case "animals":
-    dayName = "27";
+    category = "27";
     break;
   case "general":
-    dayName = "9";
+    category = "9";
     break;
-  case "sport":
-    dayName = "21";
+  case "sports":
+    category = "21";
     break;
   default:
-    dayName = "general";
+    category = "general";
     break;
 }
-
-console.log(category);
 
 async function fetchQuestions() {
   const difficulty = document.querySelector(
@@ -44,7 +45,6 @@ async function fetchQuestions() {
     const result = await fetch(`${APIUrl}`);
 
     const data = await result.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error(error);
@@ -53,8 +53,64 @@ async function fetchQuestions() {
 
 async function startGame(e) {
   e.preventDefault();
-  const questions = await fetchQuestions();
+  const { results } = await fetchQuestions();
+
+  nextQuestion(results, 0);
+
   gameForm.remove();
+}
+
+async function nextQuestion(questions, index) {
+  if (questions.length <= index) {
+    await delayTimer(500);
+    console.log("end quiz");
+    return;
+  }
+
+  const { question, correct_answer, incorrect_answers } = questions[index];
+
+  questionText.innerHTML = question;
+
+  const answers = [...incorrect_answers, correct_answer];
+
+  const html = `  <button class="btn">${answers[0]}</button>
+        <button class="btn">${answers[1]}</button>
+        <button class="btn">${answers[2]}</button>
+        <button class="btn">${answers[3]}</button>`;
+
+  answersText.innerHTML = html;
+
+  const answerBtns = Array.from(answersText.querySelectorAll(".btn"));
+
+  answerBtns.forEach((button) => {
+    button.addEventListener("click", (e) =>
+      checkAnswer(e, correct_answer, index, questions)
+    );
+  });
+}
+
+async function checkAnswer(e, answer, index, questions) {
+  const userAnswer = e.target.innerHTML;
+
+  if (userAnswer === answer) {
+    e.target.style.background = "green";
+    correctAnswers++;
+
+    await delayTimer(500);
+    nextQuestion(questions, index + 1);
+  } else {
+    e.target.style.background = "red";
+    await delayTimer(500);
+    nextQuestion(questions, index + 1);
+  }
+}
+
+function delayTimer(delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
+  });
 }
 
 gameForm.addEventListener("submit", startGame);
