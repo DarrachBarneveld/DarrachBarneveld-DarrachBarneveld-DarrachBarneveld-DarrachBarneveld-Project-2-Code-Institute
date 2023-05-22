7e8a99696969b184c907cd656e823764eea4ecc5
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-05-22 01:13:28 +0100

    updated trophy room and add script to unlock cards

---
+++ trophy.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let currentMedals = JSON.parse(localStorage.getItem("medals"));
+
+  if (!currentMedals) return;
+  else {
+    completedCategoryCheck(currentMedals);
+  }
+});
+
+function completedCategoryCheck(array) {
+  for (const obj of array) {
+    const { category, easy, medium, hard } = obj;
+    if (easy && medium && hard) {
+      const element = document.getElementById(category);
+      element.classList.add("badge-box");
+      element.classList.remove("none");
+
+      const html = ` <h3 class="state-title">${category.toUpperCase()}</h3>
+      <div class="icon">
+        <img src="assets/images/${category}.png" />
+      </div>
+      <div class="badge-container">
+        <img class="badge" src="/assets/images/bronze.png" />
+        <img class="badge" src="/assets/images/silver.png" />
+        <img class="badge" src="/assets/images/gold.png" />
+      </div>`;
+
+      element.innerHTML = html;
+    }
+  }
+}
